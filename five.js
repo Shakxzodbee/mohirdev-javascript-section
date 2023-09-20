dd7b406ee0bd24e11a2c6e78f6363c1d98bab281
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2023-09-20 16:38:07 +0500

    fifth example

---
+++ five.js
@@ -0,0 +1,10 @@
+let a = +prompt('a sonni kiriting'),
+    b = +prompt('b sonni kiriting'),
+    c = +prompt('c sonni kiriting'),
+    d = +prompt('d sonni kiriting');
+
+if(a > 0 && b > 0 && c > 0 && d > 0){
+console.log('musbat');
+}else{
+    console.log('manfiy');
+}
