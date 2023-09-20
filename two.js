aae38c3c7d87b95523e1e28ba13520252ef3f41c
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2023-09-20 16:16:18 +0500

    second example

---
+++ two.js
@@ -0,0 +1,19 @@
+const value = +prompt('son kiriting')
+
+if(value == 1 || value == '01'){
+    console.log('Dushanba');
+}else if(value == 2 || value == '02'){
+    console.log('Seshanba');
+}else if(value == 3 || value == '03'){
+    console.log('Chorshanba');
+}else if(value == 4 || value == '04'){
+    console.log('Payshanba');
+}else if(value == 5 || value == '05'){
+    console.log('Juma');
+}else if(value == 6 || value == '06'){
+    console.log('Shanba');
+}else if(value == 7 || value == '07'){
+    console.log('yakshanba');
+}else{
+    console.log('none');
+}
