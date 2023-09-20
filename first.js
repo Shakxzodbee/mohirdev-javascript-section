8c1eae06e2c2fcccf539c33a6033537afbbba98e
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2023-09-20 16:33:08 +0500

    the fourth example

---
+++ first.js
@@ -0,0 +1,10 @@
+const a = +prompt('sonni kiriting');
+if (a < 9) {
+    console.log(1)
+} else if (a < 99) {
+    console.log(2)
+} else if (a < 999) {
+    console.log(3);
+} else if (a < 9999) {
+    console.log('bu son 4 xonali sondan katta');
+}
